84f3de73455b2a78380fdf1fd3638b531f4010a5
chousawa <779583547@qq.com>
2016-12-25 20:05:16 +0800

    the second add

---
+++ ex15-3.py
@@ -0,0 +1,9 @@
+print "What's the filename?"
+filename= raw_input(">")
+txt= open(filename)
+print "Here's your file %r:"% filename
+print txt.read()
+print "Type the filename again:"
+file_again= raw_input(">")
+txt_again= open(file_again)
+print txt_again.read()
